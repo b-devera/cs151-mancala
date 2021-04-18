aa1f6aa75dca3d40c8664f007f437d1cb527bd66
Berlun Devera <berlun.devera@sjsu.edu>
2021-04-17 20:43:34 -0700

    added player and board classes, designed example template of board

---
+++ designers101-team-project/src/MancalaBoard.java
@@ -0,0 +1,17 @@
+
+public class MancalaBoard {
+	/**
+	 * This is a template of how the board should look like. We still need to
+	 * figure out how to format it so that it can work as the numbers change
+	 */
+	public void printExampleBoard() {
+		System.out.println("---------------------------------------------------------");
+		System.out.println("|      |  B6  |  B5  |  B4  |  B3  |  B2  |  B1  |      |");
+		System.out.println("|      |   4  |   4  |   4  |   4  |   4  |   4  |      |");
+		System.out.println("|  B   |-----------------------------------------|  A   |");
+		System.out.println("|  0   |-----------------------------------------|  0   |");
+		System.out.println("|      |  A1  |  A2  |  A3  |  A4  |  A5  |  A6  |      |");
+		System.out.println("|      |   4  |   4  |   4  |   4  |   4  |   4  |      |");
+		System.out.println("---------------------------------------------------------");
+	}
+}
---
+++ designers101-team-project/src/MancalaTest.java
@@ -0,0 +1,8 @@
+
+public class MancalaTest {
+	public static void main(String[] args) {
+		MancalaBoard mancalaBoard = new MancalaBoard();
+		Player[] players = new Player[2];
+		mancalaBoard.printExampleBoard();
+	}
+}
