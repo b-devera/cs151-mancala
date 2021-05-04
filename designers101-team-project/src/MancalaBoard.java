4927c1d22941ba0e790a2aee88a146291fa3b45a
Berlun Devera <berlun.devera@sjsu.edu>
2021-05-03 21:35:59 -0700

    implemented part of the observer pattern

--- designers101-team-project/src/MancalaBoard.java
+++ designers101-team-project/src/MancalaBoard.java
@@ -1,18 +1,31 @@
+import java.util.ArrayList;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class MancalaBoard {
-	/**
-	 * This is a template of how the board should look like. We still need to
-	 * figure out how to format it so that it can work as the numbers change
-	 */
-	public void printExampleBoard() {
-		System.out.println("---------------------------------------------------------");
-		System.out.println("|      |  B6  |  B5  |  B4  |  B3  |  B2  |  B1  |      |");
-		System.out.println("|      |   4  |   4  |   4  |   4  |   4  |   4  |      |");
-		System.out.println("|  B   |-----------------------------------------|  A   |");
-		System.out.println("|  0   |-----------------------------------------|  0   |");
-		System.out.println("|      |  A1  |  A2  |  A3  |  A4  |  A5  |  A6  |      |");
-		System.out.println("|      |   4  |   4  |   4  |   4  |   4  |   4  |      |");
-		System.out.println("---------------------------------------------------------");
+	ArrayList<Integer> pits;
+	ArrayList<ChangeListener> listeners;
+	
+	public MancalaBoard(ArrayList<Integer> p) {
+		pits = p;
+		listeners = new ArrayList<ChangeListener>();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public ArrayList<Integer> getPits() {
+		return (ArrayList<Integer>) (pits.clone());
+	}
+	
+	public void attach(ChangeListener c) {
+		listeners.add(c);
+	}
+	
+	public void update(int location, int value) {
+		pits.set(location, new Integer(value));
+		for (ChangeListener l: listeners) {
+			l.stateChanged(new ChangeEvent(this));
+		}
 	}
 	
 	/**
--- designers101-team-project/src/MancalaTest.java
+++ designers101-team-project/src/MancalaTest.java
@@ -1,69 +1,30 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class MancalaTest {
 	public static void main(String[] args) {
-		MancalaBoard mancalaBoard = new MancalaBoard();
-		Player[] players = new Player[2];
-		mancalaBoard.printExampleBoard();
+		ArrayList<Integer> pits = new ArrayList<Integer>();
 		
-		//All code below configures initial game screen (In Development)
-		JFrame frame = new JFrame();
-		frame.setTitle("Designers101 Mancala Game!");
-		frame.setSize(600, 400);
+		pits.add(new Integer(4));
+		pits.add(new Integer(4));
+		pits.add(new Integer(4));
+		pits.add(new Integer(4));
+		pits.add(new Integer(4));
+		pits.add(new Integer(4));
+		pits.add(new Integer(0));
+		pits.add(new Integer(4));
+		pits.add(new Integer(4));
+		pits.add(new Integer(4));
+		pits.add(new Integer(4));
+		pits.add(new Integer(4));
+		pits.add(new Integer(4));
+		pits.add(new Integer(0));
 		
-		TitlePanel titlePanel = new TitlePanel();
-		titlePanel.setBackground(Color.BLUE);
-		
-		JPanel stylePanel = new JPanel();
-		
-		JLabel styleChoiceLabel = new JLabel("Choose Board Style:");
-		
-		JTextPane boardArea = new JTextPane();
-		boardArea.setEditable(false);
-		
-		//Creates button for styleA and sets the style when pressed
-		JButton styleButtonA = new JButton("Style A");
-		styleButtonA.addActionListener(new
-				ActionListener()
-				{
-					public void actionPerformed(ActionEvent event)
-					{
-						mancalaBoard.setStyle(new BoardStyleA());
-					}
-				});
-		
-		//Creates button for styleB and sets the style when pressed
-		JButton styleButtonB = new JButton("Style B");
-		styleButtonB.addActionListener(new
-				ActionListener()
-				{
-					public void actionPerformed(ActionEvent event)
-					{
-						mancalaBoard.setStyle(new BoardStyleB());
-					}
-				});
-		
-		stylePanel.add(styleChoiceLabel);
-		stylePanel.add(styleButtonA);
-		stylePanel.add(styleButtonB);
-		
-		//Below code for boardArea sets a basic game board for example display
-		boardArea.setText("---------------------------------------------------------\n");
-		boardArea.setText(boardArea.getText() + "|      |  B6  |  B5  |  B4  |  B3  |  B2  |  B1  |      |\n");
-		boardArea.setText(boardArea.getText() + "|      |   4  |   4  |   4  |   4  |   4  |   4  |      |\n");
-		boardArea.setText(boardArea.getText() + "|  B   |-----------------------------------------|  A   |\n");
-		boardArea.setText(boardArea.getText() + "|  0   |-----------------------------------------|  0   |\n");
-		boardArea.setText(boardArea.getText() + "|      |  A1  |  A2  |  A3  |  A4  |  A5  |  A6  |      |\n");
-		boardArea.setText(boardArea.getText() + "|      |   4  |   4  |   4  |   4  |   4  |   4  |      |\n");
-		boardArea.setText(boardArea.getText() + "---------------------------------------------------------\n");
-		
-		frame.add(titlePanel, BorderLayout.CENTER);
-		frame.add(stylePanel, BorderLayout.NORTH);
-		frame.add(boardArea, BorderLayout.SOUTH);
-		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		MancalaBoard model = new MancalaBoard(pits);
+		MancalaFrame mancalaFrame = new MancalaFrame(model);
+		model.attach(mancalaFrame);
 	}
 }
